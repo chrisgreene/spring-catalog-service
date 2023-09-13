c6e54875116ac8cdaf5064d67b28546df2286139
Chris Greene <chrisgreene@gmail.com>
2023-09-13 15:07:50 -0600

    edit commit stage

--- src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -1,13 +1,18 @@
 package com.polarbookshop.catalogservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CatalogServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CatalogServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogServiceApplication.class, args);
+    }
 }
+
